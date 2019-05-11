c86365ffc185b3d5dfbca93545b8c45749bf1904
Noe Huaccharaque <noemk2@gmail.com>
2019-05-11 11:00:10 -0500

    metodos y construtores

--- src/com/noe/amazonviewer/Main.java
+++ src/com/noe/amazonviewer/Main.java
@@ -1,10 +1,16 @@
 package com.noe.amazonviewer;
 
+import com.noe.amazonviewer.model.Movie;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Movie movie = new Movie();
+		movie.title="Coco";
+		movie.duration=120;
+
 		int exit = 0;
 		do {
 			
@@ -19,8 +25,38 @@ public class Main {
 			
 			//Leer la respuesta del usuario
 			
-			
-			
+						//Leer la respuesta del usuario
+						int response = 0;
+						switch (response) {
+							case 0:
+								//salir
+								
+								break;
+							case 1:
+								
+								break;
+							case 2:
+								
+								break;
+							case 3:
+								
+								break;
+							case 4:
+								
+								break;
+							case 5:
+								
+								break;
+							case 6:
+								
+								break;
+				
+							default:
+								System.out.println();
+								System.out.println("....¡¡Selecciona una opción!!....");
+								System.out.println();
+								break;
+						}
 			
 		}while(exit != 0);
 		
