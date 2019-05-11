95ae62f13c32351fa2ae61182b7d6e0a07739ae2
Noe Huaccharaque <noemk2@gmail.com>
2019-05-11 18:28:50 -0500

    terminando private y sobrecargando constructores

--- src/com/noe/amazonviewer/model/Magazine.java
+++ src/com/noe/amazonviewer/model/Magazine.java
@@ -3,10 +3,18 @@ package com.noe.amazonviewer.model;
 import java.util.Date;
 
 public class Magazine {
-	int id;
-	String title;
-	Date edititionDate;
-	String editorial;
-	String[] authors;
+	private int id;
+	private String title;
+	private Date edititionDate;
+	private String editorial;
+	private String[] authors;
+
+
+	public Magazine(String title, Date edititionDate, String editorial){
+		super();
+		this.title=title;
+		this.edititionDate=edititionDate;
+		this.editorial=editorial;
+	}
 
 }
--- src/com/noe/amazonviewer/model/Serie.java
+++ src/com/noe/amazonviewer/model/Serie.java
@@ -2,17 +2,23 @@ package com.noe.amazonviewer.model;
 
 public class Serie {
 	
-	int id;
-	String title;
-	String genre;
-	String creator;
-	int duration;
-	short year;
-	boolean viewed;
-	int timeViewed;
+	private int id;
+	private String title;
+	private String genre;
+	private String creator;
+	private int duration;
+	private short year;
+	private boolean viewed;
+	private int timeViewed;
 	
 	int sessionQuantity;
 	//chapters[]
+	public Serie(String title, String genre, int duration){
+		super();
+		this.title=title;
+		this.genre=genre;
+		this.duration=duration;
+	}
 	
 
 }
