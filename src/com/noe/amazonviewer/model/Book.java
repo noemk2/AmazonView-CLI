05302d4e3e79a112d081ccf82c62aff00101d723
Noe Huaccharaque <noemk2@gmail.com>
2019-05-12 08:27:51 -0500

    Aplicando herencia en book, magazine de Publication

--- src/com/noe/amazonviewer/model/Book.java
+++ src/com/noe/amazonviewer/model/Book.java
@@ -1,135 +1,43 @@
 package com.noe.amazonviewer.model;
 
-import java.util.Date;
+public class Book extends Publication{
 
-public class Book {
-	private int id;
-	private String title;
-	private Date edititionDate;
-	private String editorial;
-	private String[] authors;
+	private int id;	
 	private String isbn;
 	private boolean readed;
 	private int timeReaded;
-
-	public Book(String title,Date edititionDate, String editorial, String isbn){
-		super();
-		this.title=title;
-		this.edititionDate=edititionDate;
-		this.editorial=editorial;
+		
+	public Book(String title, Date editionDate, String editorial, String isbn){
+		super(title, editionDate, editorial);
 		this.isbn=isbn;
 	}
 
-	/**
-	 * @return the id
-	 */
-	public int getId() {
+	public intgetId(){
 		return id;
 	}
 
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	/**
-	 * @return the title
-	 */
-	public String getTitle() {
-		return title;
-	}
-
-	/**
-	 * @param title the title to set
-	 */
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	/**
-	 * @return the edititionDate
-	 */
-	public Date getEdititionDate() {
-		return edititionDate;
-	}
-
-	/**
-	 * @param edititionDate the edititionDate to set
-	 */
-	public void setEdititionDate(Date edititionDate) {
-		this.edititionDate = edititionDate;
-	}
-
-	/**
-	 * @return the editorial
-	 */
-	public String getEditorial() {
-		return editorial;
-	}
-
-	/**
-	 * @param editorial the editorial to set
-	 */
-	public void setEditorial(String editorial) {
-		this.editorial = editorial;
-	}
-
-	/**
-	 * @return the authors
-	 */
-	public String[] getAuthors() {
-		return authors;
-	}
-
-	/**
-	 * @param authors the authors to set
-	 */
-	public void setAuthors(String[] authors) {
-		this.authors = authors;
-	}
-
-	/**
-	 * @return the isbn
-	 */
-	public String getIsbn() {
+	public String getIsbn(){
 		return isbn;
 	}
 
-	/**
-	 * @param isbn the isbn to set
-	 */
-	public void setIsbn(String isbn) {
+	public voidsetIsbn(String isbn){
 		this.isbn = isbn;
 	}
 
-	/**
-	 * @return the readed
-	 */
-	public boolean isReaded() {
+	public booleanisReaded(){
 		return readed;
 	}
 
-	/**
-	 * @param readed the readed to set
-	 */
-	public void setReaded(boolean readed) {
+	public voidsetReaded(boolean readed){
 		this.readed = readed;
 	}
 
-	/**
-	 * @return the timeReaded
-	 */
-	public int getTimeReaded() {
+	public intgetTimeReaded(){
 		return timeReaded;
 	}
 
-	/**
-	 * @param timeReaded the timeReaded to set
-	 */
-	public void setTimeReaded(int timeReaded) {
+	public voidsetTimeReaded(int timeReaded){
 		this.timeReaded = timeReaded;
 	}
-
+	
 }
--- src/com/noe/amazonviewer/model/Magazine.java
+++ src/com/noe/amazonviewer/model/Magazine.java
@@ -1,90 +1,13 @@
 package com.noe.amazonviewer.model;
 
-import java.util.Date;
-
-public class Magazine {
-	private int id;
-	private String title;
-	private Date edititionDate;
-	private String editorial;
-	private String[] authors;
-
-
-	public Magazine(String title, Date edititionDate, String editorial){
-		super();
-		this.title=title;
-		this.edititionDate=edititionDate;
-		this.editorial=editorial;
+public class Magazine extends Publication{
+	privateint id;		
+			
+	public Magazine(String title, Date editionDate, String editorial){
+		super(title, editionDate, editorial);
 	}
 
-	/**
-	 * @return the id
-	 */
-	public int getId() {
+	publicintgetId(){
 		return id;
 	}
-
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	/**
-	 * @return the title
-	 */
-	public String getTitle() {
-		return title;
-	}
-
-	/**
-	 * @param title the title to set
-	 */
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	/**
-	 * @return the edititionDate
-	 */
-	public Date getEdititionDate() {
-		return edititionDate;
-	}
-
-	/**
-	 * @param edititionDate the edititionDate to set
-	 */
-	public void setEdititionDate(Date edititionDate) {
-		this.edititionDate = edititionDate;
-	}
-
-	/**
-	 * @return the editorial
-	 */
-	public String getEditorial() {
-		return editorial;
-	}
-
-	/**
-	 * @param editorial the editorial to set
-	 */
-	public void setEditorial(String editorial) {
-		this.editorial = editorial;
-	}
-
-	/**
-	 * @return the authors
-	 */
-	public String[] getAuthors() {
-		return authors;
-	}
-
-	/**
-	 * @param authors the authors to set
-	 */
-	public void setAuthors(String[] authors) {
-		this.authors = authors;
-	}
-
 }
